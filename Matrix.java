cdc09635af03cbb8c763ec3d929054c147d9c35f
bsalchert <salchert@hartford.edu>
2022-11-07 09:06:45 -0500

    Added method printMatrix

--- Matrix.java
+++ Matrix.java
@@ -1,13 +1,30 @@
 public class Matrix {
     private int[][] matrix;
 
-    //------------------------------------------------------
-    //  Constructor: Sets up a matrix object with dimension
-    //  size.
-    //------------------------------------------------------
+    //------------------------------------------------------------------
+    //  Constructor: Sets up a matrix object with dimension size.
+    //------------------------------------------------------------------
     public Matrix(int size) {
         matrix = new int[size][size];
 
-        System.out.println("\tGenerated a " + size + " x " + size + " matrix.");
+        System.out.println("\tGenerated a " + size + " x " + size +
+                           " matrix.\n");
+    }
+
+    //------------------------------------------------------------------
+    //  Prints out the matrix to the terminal.
+    //------------------------------------------------------------------
+    public void printMatrix() {
+        System.out.println("\tThe empty matrix:");
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix.length; column++) {
+                System.out.print("\t" + matrix[row][column]);
+            }
+
+            System.out.println("");
+        }
+
+        System.out.println();
     }
 }
