30fc3a3a41b99a5756dc0ed0b6deeb28f0c8e1ce
bsalchert <salchert@hartford.edu>
2022-11-07 08:54:51 -0500

    Added user input and check for too small size

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
 public class RunMatrix {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int matrixSize;
 
+        System.out.println();
+        System.out.print("\tEnter the dimension of the matrix: ");
+        matrixSize = input.nextInt();
+
+        while (matrixSize < 2) {
+            System.out.println("\tInvalid -- input must be at least 2.");
+            System.out.println();
+
+            System.out.print("\tEnter the dimension of the matrix: ");
+            matrixSize = input.nextInt();
+        }
+    }
 }
