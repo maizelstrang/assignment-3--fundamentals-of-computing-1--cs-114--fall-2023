28bb4148b25876c49947afed309d2c3baf730e9c
bsalchert <salchert@hartford.edu>
2022-11-07 08:48:51 -0500

    Added member and constructor for Matrix

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,13 @@
 public class Matrix {
+    private int[][] matrix;
 
+    //------------------------------------------------------
+    //  Constructor: Sets up a matrix object with dimension
+    //  size.
+    //------------------------------------------------------
+    public Matrix(int size) {
+        matrix = new int[size][size];
+
+        System.out.println("\tGenerated a " + size + " x " + size + " matrix.");
+    }
 }
