b2f26ec0b0b68535ad7d04a5ba9a888ce257616a
justin-jaques <87876422+V1ridescent@users.noreply.github.com>
2022-11-11 21:45:51 -0500

    yes

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,12 @@
+/*
+ * I would just like to note that writing this class killed almost every individual brain cell that I have,
+ * similarly to sniffing one of those extremely synthetic smelling markers for an extended period of time.
+ * Completing this felt so good, but I know that eventually I'm gonna look at this and think "hey this was really bad,
+ * I should've just quit programming". Anywho, monologuing my stress levels while making these assignements in the form
+ * of comments is suprisingly very relieving. Enjoy.
+ */
+
+
 public class Matrix {
     private int size = 0;
     private int matrix[][];
@@ -10,7 +19,7 @@ public class Matrix {
      public void populate() {
         for(int i = 0; i < size; i++) {
             for(int j = 0; j < size; j++) {
-                matrix[i][j] = (i * size) + j;
+                matrix[i][j] = (i * size) + j + 1;
             }
            }
 
@@ -27,11 +36,10 @@ public class Matrix {
 
       public void transpose() {
           for(int i = 0; i < size; i++) {
-              for(int j = 0; j < i; j++) {
-
-                  int t = matrix[i][j];
-                  matrix[i][j] = matrix[i][size - j - 1];
-                  matrix[i][size - j - 1] = t;
+              for(int j = 0; j < i+1; j++) {
+                int t = matrix[i][size - j - 1];
+                matrix[i][size-j-1] = matrix[i][j];
+                matrix[i][j] = t;
               }
 
           }
--- RunMatrix.java
+++ RunMatrix.java
@@ -7,6 +7,11 @@ public class RunMatrix {
         System.out.println("Please enter a number for your matrix:");
         int size = input.nextInt();
 
+        if(size < 2) {
+            System.out.println("Please enter an integer greater than or equal to 2");
+            System.exit(1);
+        }
+
         Matrix matrix = new Matrix(size);
         System.out.println();
         System.out.println("You requested a Matrix with size: " + size + " x " + size);
