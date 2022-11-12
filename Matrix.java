90e04158d5bbd9346efb8146edf7da47f6dd755c
justin-jaques <87876422+V1ridescent@users.noreply.github.com>
2022-11-11 20:17:26 -0500

    Made things better :)

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,40 @@
 public class Matrix {
+    private int size = 0;
+    private int matrix[][];
+    public Matrix(int iSize) {
+       size = iSize;
+       matrix = new int[size][size];
 
-}
+     }
+
+     public void populate() {
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                matrix[i][j] = (i * size) + j;
+            }
+           }
+
+     }
+
+     public void print() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print("\t" + matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+     }
+
+      public void transpose() {
+          for(int i = 0; i < size; i++) {
+              for(int j = 0; j < i; j++) {
+
+                  int t = matrix[i][j];
+                  matrix[i][j] = matrix[i][size - j - 1];
+                  matrix[i][size - j - 1] = t;
+              }
+
+          }
+      }
+
+  }
--- RunMatrix.java
+++ RunMatrix.java
@@ -1,5 +1,31 @@
 import java.util.Scanner;
 
 public class RunMatrix {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String args[]) {
+        System.out.println("Please enter a number for your matrix:");
+        int size = input.nextInt();
+
+        Matrix matrix = new Matrix(size);
+        System.out.println();
+        System.out.println("You requested a Matrix with size: " + size + " x " + size);
+        System.out.println();
+        System.out.println("Regular Matrix:");
+        System.out.println();
+        matrix.print();
+        System.out.println();
+        matrix.populate();
+        System.out.println("Populated Matrix:");
+        System.out.println();
+        matrix.print();
+        System.out.println();
+        matrix.transpose();
+        System.out.println("Transposed Matrix:");
+        System.out.println();
+        matrix.print();
+        System.out.println();
+
+    }
 
 }
