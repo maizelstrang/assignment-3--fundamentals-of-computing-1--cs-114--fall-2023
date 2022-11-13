6187332ec7e777d8655fe3696321fb77e0df8e86
Aiden Kiss <aidenk.7103@gmail.com>
2022-11-13 16:22:28 -0500

    added be userMatrix object

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
-
-
 public class RunMatrix {
+    public static void main(String[] args){
+        Matrix userMatrix;
+        userMatrix = new Matrix();
 
 
 
+
+    }
 }
-
