aaf659d1ec3a7b150adba595b721ce7a62a55c57
justin-jaques <87876422+V1ridescent@users.noreply.github.com>
2022-11-13 17:18:00 -0500

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -8,6 +8,7 @@
 
 
 public class Matrix {
+    private String ANSI_YELLOW = "\u001B[33m";
     private int size = 0;
     private int matrix[][];
     public Matrix(int iSize) {
@@ -29,6 +30,7 @@ public class Matrix {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print("\t" + matrix[i][j] + " ");
+                System.out.print(ANSI_YELLOW);
             }
             System.out.println();
         }
