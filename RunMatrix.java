40e71795fa19d379a87ccd25bab9073799f4f8c7
Aiden Kiss <aidenk.7103@gmail.com>
2022-11-13 16:25:27 -0500

    added getSize to runmatrix

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class RunMatrix {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
         Matrix userMatrix;
         userMatrix = new Matrix();
 
