60a26bbfb0dc670366f3d984b5668991750cc513
Aiden Kiss <aidenk.7103@gmail.com>
2022-11-14 09:46:56 -0500

    added makefile indent style to tab

--- RunMatrix.java
+++ RunMatrix.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class RunMatrix {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+
 
         Matrix userMatrix;
         userMatrix = new Matrix();
