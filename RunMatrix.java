a898c94dc4b58f57ecf385431d946bb3d7e4ac25
alayala2025 <alayala@hartford.edu>
2022-11-14 08:57:55 -0500

    adding inputs

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,5 +1,17 @@
 import java.util.Scanner;
 
 public class RunMatrix {
+    public static void main(String arg[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the Matrix");
+        int r = sc.nextInt();
+
+        System.out.println("Re-enter the number selected");
+        int c = sc.nextInt();
+
+        int[][]table = new int[r][c];
+    }
 
 }
