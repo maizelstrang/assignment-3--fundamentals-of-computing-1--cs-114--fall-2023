1134f5bdb8feff867f3d8527828c969c122c38cf
alayala2025 <alayala@hartford.edu>
2022-11-14 13:04:22 -0500

    Printing table

--- RunMatrix.java
+++ RunMatrix.java
@@ -5,13 +5,40 @@ public class RunMatrix {
 
         Scanner sc = new Scanner(System.in);
 
+        //Requests from the user a positive number
+        //representing the size of a matrix
         System.out.println("Enter the size of the Matrix");
-        int r = sc.nextInt();
+        int rc = sc.nextInt();
+        System.out.println();
+        System.out.println();
 
-        System.out.println("Re-enter the number selected");
-        int c = sc.nextInt();
+        //System.out.println("Re-enter the number selected");
+        //int c = sc.nextInt();
 
-        int[][]table = new int[r][c];
+        //Load table with values
+        int[][]table = new int[rc][rc];
+        for (int row = 0; row < table.length; row++) {
+            for (int col = 0; col < table[row].length; col++) {
+                table[row][col] = (row * rc) + col;
+            }
+
+        }
+        //Print the table consistant of 0
+
+
+        //Print the table
+        for (int row = 0; row < table.length; row++) {
+            for (int col = 0; col < table[row].length; col++){
+                System.out.print(table[row][col] + "\t");
+            }
+                System.out.println();
+        }
+
+    //    for (int row = 0; row < table.length; row++) {
+    //        for (int col = 0; col < table[row].length; col++){
+    //            System.out.print(table[col][row] + "\t");
+    //        }
+    //            System.out.println();
+    //    }
     }
-
 }
