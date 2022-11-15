b38180ec75cec416f2c6aefdf3bec92fe6fd5b0b
justin-jaques <87876422+V1ridescent@users.noreply.github.com>
2022-11-15 13:55:14 -0500

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,11 +1,3 @@
-/*
- * I would just like to note that writing this class killed almost every individual brain cell that I have,
- * similarly to sniffing one of those extremely synthetic smelling markers for an extended period of time.
- * Completing this felt so good, but I know that eventually I'm gonna look at this and think "hey this was really bad,
- * I should've just quit programming". Anywho, monologuing my stress levels while making these assignements in the form
- * of comments is suprisingly very relieving. Enjoy.
- */
-
 
 public class Matrix {
     private String ANSI_YELLOW = "\u001B[33m";
@@ -30,7 +22,7 @@ public class Matrix {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print("\t" + matrix[i][j] + " ");
-                System.out.print(ANSI_YELLOW);
+
             }
             System.out.println();
         }
