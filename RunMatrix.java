e8e8289741bd037d4e2c928224ddb9bd6c54d3b7
GreenMatt413 <111987332+GreenMatt413@users.noreply.github.com>
2022-11-15 16:23:22 -0500

    Program creates matrix square (with zeros)

--- RunMatrix.java
+++ RunMatrix.java
@@ -10,8 +10,11 @@ public class RunMatrix {
         System.out.println("Please enter the size of your matrix: ");
         matrixSize = scan.nextInt();
 
-        for (int column=0; column<matrixSize; column++){
-            System.out.print("0 ");
+        for (int column=0; column<matrixSize; column++) {
+            for (int row=0; row<matrixSize; row++) {
+            System.out.print("0  ");
+            }
+            System.out.print("\n");
         }
 
 
