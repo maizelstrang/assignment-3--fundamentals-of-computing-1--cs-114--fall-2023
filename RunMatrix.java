c962f378f01b4fa1592da2529296c8484e1d4fc3
GreenMatt413 <111987332+GreenMatt413@users.noreply.github.com>
2022-11-15 16:31:15 -0500

    Add more text describing program's actions

--- RunMatrix.java
+++ RunMatrix.java
@@ -5,11 +5,13 @@ public class RunMatrix {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int matrixSize, matrixLength;
+        int matrixSize;
 
         System.out.println("Please enter the size of your matrix: ");
         matrixSize = scan.nextInt();
 
+        System.out.print("Your matrix is: " + matrixSize); System.out.println("x" +matrixSize);
+        System.out.println("Printing matrix with default values...");
         for (int column=0; column<matrixSize; column++) {
             for (int row=0; row<matrixSize; row++) {
             System.out.print("0  ");
