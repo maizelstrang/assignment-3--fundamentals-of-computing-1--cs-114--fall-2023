dc5d78d4a2d219438384d77496ef3f8cd8905edd
alayala2025 <alayala@hartford.edu>
2022-11-15 12:23:02 -0500

    Experimenting

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;
+
 public class Matrix {
 
+
+    public static void main(String args[]) {
+        int a [][] = new int[3][3],i,j,t;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Matrix Elements");
+        for (i=0;i<3;i++){
+            for (j = 0; j < 3; j++){
+                System.out.print("Enter Number in Pocket["+i+"]["+j+"]");
+                a[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Matrix is ....");
+        for (i = 0; i < 3; i++){
+            for(j=0;j<3;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+        for ( i = 0; i<3; i++){
+            t = a[0][i];
+            a[0][i]= a[3][i];
+            a[3][i] = t;
+
+        }
+        System.out.println("After Swapping Row of Matrix");
+        for (i=0;i<3;i++){
+            for (j=0;j<3;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class RunMatrix {
+
     public static void main(String arg[]){
 
         Scanner sc = new Scanner(System.in);
 
         //Requests from the user a positive number
-        //representing the size of a matrix.
+        //representing the size of a matrix
         System.out.println("Enter the size of the Matrix");
         int rc = sc.nextInt();
         System.out.println();
@@ -15,7 +16,7 @@ public class RunMatrix {
         //System.out.println("Re-enter the number selected");
         //int c = sc.nextInt();
 
-        //Load table with values.
+        //Load table with values
         int[][]table = new int[rc][rc];
         for (int row = 0; row < table.length; row++) {
             for (int col = 0; col < table[row].length; col++) {
@@ -23,38 +24,24 @@ public class RunMatrix {
             }
 
         }
-        //Print the table consistant of '0's.
+        //Print the table consistant of '0's
         System.out.println("Here is the table consistent of '0'");
-
-
-        //Print the original table.
-        System.out.println("Here is the Original table");
         for (int row = 0; row < table.length; row++) {
-            for (int col = 0; col < table[row].length; col++){
+            for (int col = 0; col < table[row].length; col++) {
+                table[row][col] = 0;
                 System.out.print(table[row][col] + "\t");
             }
                 System.out.println();
         }
-
-        //Print the Inverse Table.
-        System.out.print("Here is the Reverse table");
-        for (int i = 0; i < table.length/2; i++){
-            int temp = table[i];
-            table[i] = table[table.length - i - 1];
-            table[table.length - i - 1] = temp;
-        }
-        for (int j = 0; j < table.length; j++){
-            for (int i = 0; i < table[j].length / 2; i++){
-                int temp = table[j][i];
-                table[j][i] = table[j][table[j].length - i - 1];
-                table[j][table[j].length - i - 1] = temp;
+        //Print the table
+        System.out.println("Here is the Original table");
+        for (int row = 0; row < table.length; row++) {
+            for (int col = 0; col < table[row].length; col++){
+                table[row][col] = row * col;
+                System.out.print(table[row][col] + "\t");
             }
+                System.out.println();
+
         }
-    //    for (int row = 0; row < table.length; row++) {
-    //        for (int col = 0; col < table[row].length; col++){
-    //            System.out.print(table[col][row] + "\t");
-    //        }
-    //            System.out.println();
-    //    }
     }
 }
