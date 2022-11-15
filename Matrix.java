d11a91911988bc7838739b9026c47df05a7ee443
GreenMatt413 <111987332+GreenMatt413@users.noreply.github.com>
2022-11-14 19:37:14 -0500

    Include user input

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Matrix {
+
+    public static void main(String[] args){
+
+    Scanner scan = new Scanner(System.in);
+    int matrixSize;
+
+
+    System.out.println("Please enter the size of your matrix: ");
+    matrixSize = scan.nextInt();
+
+    }
 
 }
