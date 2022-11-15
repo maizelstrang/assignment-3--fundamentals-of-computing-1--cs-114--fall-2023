b6cb39cec5c555b64eff6217b153a705369ad1e1
Aiden Kiss <aidenk.7103@gmail.com>
2022-11-15 16:53:24 -0500

    highlight implemented properly

--- Matrix.java
+++ Matrix.java
@@ -12,14 +12,36 @@ public class Matrix {
     }
 
 //-----------------------------------------------------------------------------------------------------
-public void printMatrix(){
-    int diagongalHighlight = matrixSize-1;
-    for (int row = 0; row < matrixSize ; row++ ){
-            for (int col = 0; col < matrixSize; col++){
-                matrix[row][col] = 0;
+    public void printMatrix(){
+        int diagongalHighlight = matrixSize-1;
+        for (int row = 0; row < matrixSize ; row++ ){
+                for (int col = 0; col < matrixSize; col++){
+                    matrix[row][col] = 0;
+                }
+        }
+        System.out.println("\nPrinting matrix with default values:");
+            for (int row = 0; row< matrixSize; row++){
+                System.out.println();
+                for(int col = 0; col < matrixSize; col++){
+                    if (diagongalHighlight == col){
+                    System.out.printf("%s%d%s", COLOR, matrix[row][col], "\t");
+                    diagongalHighlight--;
+                    } else {
+                    System.out.printf("%s%d%s", RESET, matrix[row][col], "\t");
+                    }
+                }
             }
     }
-    System.out.println("\nPrinting matrix with default values:");
+
+//-----------------------------------------------------------------------------------------------------
+    public void populateMatrix(){
+        int diagongalHighlight = matrixSize-1;
+        for (int row = 0; row < matrixSize ; row++ ){
+            for (int col = 0; col < matrixSize; col++){
+                matrix[row][col] += (col+row);
+            }
+        }
+        System.out.println("\nPrinting matrix with default values:");
         for (int row = 0; row< matrixSize; row++){
             System.out.println();
             for(int col = 0; col < matrixSize; col++){
@@ -31,24 +53,5 @@ public void printMatrix(){
                 }
             }
         }
-}
-
-//-----------------------------------------------------------------------------------------------------
-public void populateMatrix(){
-        for (int row = 0; row < matrixSize ; row++ ){
-            for (int col = 0; col < matrixSize; col++){
-                matrix[row][col] += (col+row);
-            }
-        }
-        System.out.println("\nPrinting matrix with default values:");
-        for (int row = 0; row< matrix.length; row++){
-            System.out.println();
-
-            for(int col = 0; col < matrix.length; col++){
-            System.out.printf("%d%s", matrix[row][col], "\t");
-            }
-        }
     }
-
-
 }
