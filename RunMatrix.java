da61878ebadd8d3f03c8c89700df91c65e9839d7
K-nicholas29 <111987510+K-nicholas29@users.noreply.github.com>
2022-11-15 18:45:38 -0500

    Update RunMatrix.java

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,4 +1,7 @@
+import java.nio.file.attribute.UserPrincipal;
 import java.util.Scanner;
+
+import javax.sql.rowset.spi.SyncResolver;
 
 public class RunMatrix {
     // -----------------------------------------------------------------
@@ -9,26 +12,15 @@ public class RunMatrix {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println();
+        System.out.println("Please enter a positive integer: ");
+        int Userinput = scanner.nextInt();
+
+        int Squared = Userinput * Userinput;
+        System.out.println("Your number squared is: " + Squared);
 
 
-        int[][] table = new int[5][10];
-
-        // Load the table with values
-        for (int row = 0; row < table.length; row++) {
-            for (int col = 0; col < table[row].length; col++) {
-                table[row][col] = (row * 10) + col;
-            }
-        }
 
 
-        // Print the table
-        for (int row = 0; row < table.length; row++) {
-            for (int col = 0; col < table[row].length; col++) {
-                System.out.print (table[row][col] + "\t");
-            }
 
-                System.out.println();
-        }
     }
 }
