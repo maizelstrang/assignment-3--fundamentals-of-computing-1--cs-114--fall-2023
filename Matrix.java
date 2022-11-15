ec64fce371740f6ab64775dd0a203be878cc8eee
GreenMatt413 <111987332+GreenMatt413@users.noreply.github.com>
2022-11-15 16:19:18 -0500

    move code over to RunMatrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,16 +1,4 @@
-import java.util.Scanner;
 
 public class Matrix {
-
-    public static void main(String[] args){
-
-    Scanner scan = new Scanner(System.in);
-    int matrixSize;
-
-
-    System.out.println("Please enter the size of your matrix: ");
-    matrixSize = scan.nextInt();
-
-    }
 
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,4 +2,20 @@ import java.util.Scanner;
 
 public class RunMatrix {
 
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int matrixSize, matrixLength;
+
+        System.out.println("Please enter the size of your matrix: ");
+        matrixSize = scan.nextInt();
+
+        for (int column=0; column<matrixSize; column++){
+            System.out.print("0 ");
+        }
+
+
+
+
+        }
 }
