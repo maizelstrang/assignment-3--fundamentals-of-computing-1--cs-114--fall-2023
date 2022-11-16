ee58d1d90b41e022a6960ab7deb37a24ea4dc447
11swp03 <papallo@hartford.edu>
2022-11-15 20:12:55 -0500

    add populatetable method

--- Matrix.java
+++ Matrix.java
@@ -3,4 +3,11 @@ public class Matrix {
         int[][] table = new int[5][5];
         populateTable();
     }
+    public populateTable() {
+        for (int row = 0; row < table.length; row++){
+            for (int col = 0; col < table[row].length; col++){
+                table[row][col] = row*10+col;
+            }
+        }
+    }
 }
