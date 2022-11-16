44e135fb0405fc06a7be061829021e1d400f3c87
alayala2025 <alayala@hartford.edu>
2022-11-16 08:20:24 -0500

    adjustment

--- Matrix.java
+++ Matrix.java
@@ -1,37 +1,26 @@
-import java.util.Scanner;
-
+// Create a Constructor
+//Method call for the matrix class, setting base parameters.
 public class Matrix {
+    // starting tablle
+    int[][] table;
 
+    //inputing rows and columns
+    Matrix(int r,int c){
+        table = new int[r][c];
 
-    public static void main(String args[]) {
-        int a [][] = new int[3][3],i,j,t;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Matrix Elements");
-        for (i=0;i<3;i++){
-            for (j = 0; j < 3; j++){
-                System.out.print("Enter Number in Pocket["+i+"]["+j+"]");
-                a[i][j]=sc.nextInt();
+        for(int row = 0; row < table.length ; row++){
+            for(int col = 0 ; col < table[row].length ; col++){
+                table[row][col] = 0;
             }
-        }
-        System.out.println("Matrix is ....");
-        for (i = 0; i < 3; i++){
-            for(j=0;j<3;j++){
-                System.out.print(a[i][j]+" ");
-            }
-            System.out.println();
-        }
-        for ( i = 0; i<3; i++){
-            t = a[0][i];
-            a[0][i]= a[3][i];
-            a[3][i] = t;
-
-        }
-        System.out.println("After Swapping Row of Matrix");
-        for (i=0;i<3;i++){
-            for (j=0;j<3;j++){
-                System.out.print(a[i][j]+" ");
-            }
-            System.out.println();
         }
     }
+
+    public void printMatrix(){
+        for(int row = 0;row < table.length ; row++){
+            for(int col = 0 ; col < table[row].length ; col++){
+            System.out.print(table[row][col] + " ");
+        }
+        System.out.println();
+    }
+}
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,62 +2,75 @@ import java.util.Scanner;
 
 public class RunMatrix {
 
-    public static void main(String arg[]){
+    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        //scanner is place so that it focus just in the number of the user input.
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Enter the size of the Matrix:\n");
+        //define size as the input in order to set the matrix size
+        int size = scan.nextInt();
+        //Create a matrix object that can runs methods, when calling this line it calls the construct
+        Matrix m1 = new Matrix(size,size);
+        m1.printMatrix();
+    }
+}
         //Requests from the user a positive number
         //representing the size of a matrix
-        System.out.println("Enter the size of the Matrix");
-        int rc = sc.nextInt();
-        System.out.println();
-        System.out.println();
+        //System.out.println("Enter the size of the Matrix");
+        //int a = sc.nextInt();
+        //System.out.println();
+        //System.out.println();
+
+        //int value = 0;
 
         //System.out.println("Re-enter the number selected");
         //int c = sc.nextInt();
         //rc is the size of the matrix in rows and columns
         //Load table with values
-        int[][]table = new int[rc][rc];
-        for (int row = 0; row < table.length; row++) {
-            for (int col = 0; col < table[row].length; col++) {
-                table[row][col] = (row * rc) + col;
-            }
+        //int[][] table = new int[a][a];
+        //for (int row = 0; row < table.length; row++) {
+            //for (int col = 0; col < table[row].length; col++) {
+                //table[row][col] = value++; // (row * rc) + col;
+            //}
 
-        }
+        //}
         //Print the table consistant of '0's
-        System.out.println("Here is the table consistent of '0'");
-        for (int row = 0; row < table.length; row++) {
-            for (int col = 0; col < table[row].length; col++) {
-                table[row][col] = 0;
-                System.out.print(table[row][col] + "\t");
-            }
-                System.out.println();
-        }
+        // System.out.println("Here is the table consistent of '0'");
+        // for (int row = 0; row < table.length; row++) {
+        //     for (int col = 0; col < table[row].length; col++) {
+        //         table[row][col] = 0;
+        //         System.out.print(table[row][col] + "\t");
+        //     }
+        //         System.out.println();
+        // }
         //Print the original table
-        System.out.println("Here is the Original table");
-        int count = 0;
-        for (int row = 0; row < table.length; row++) {
-            for (int col = 0; col < table[row].length; col++){
-                //table[row][col] = row + col;
-                table[row][col] = count;
-                count++;
-                System.out.print(table[row][col] + "\t");
-            }
-                System.out.println();
+        //System.out.println("Here is the Original table");
+        //int count = 0;
+        //for (int row = 0; row < table.length; row++) {
+            //for (int col = 0; col < table[row].length; col++){
 
-        }
+                //System.out.println(table[row][col]);
+
+                //table[row][col] = row + col;
+                //table[row][col] = count;
+                //count++;
+                //System.out.print(table[row][col] + "\t");
+            //}
+                //System.out.println();
+
+
         //Print the Reverse table
-        System.out.println("Here is the Reverse table");
-        count = (rc*rc)-1;
-        for (int row = 0; row < table.length; row++) {
-            for (int col = 0; col < table[row].length; col++){
+        //System.out.println("Here is the Reverse table");
+        //count = (a*a)-1;
+        //for (int row = 0; row < table.length; row++) {
+            //for (int col = 0; col < table[row].length; col++){
                 //table[row][col] = row + col;
-                table[row][col] = count;
-                count--;
-                System.out.print(table[row][col] + "\t");
-            }
-                System.out.println();
+                //table[row][col] = count;
+                //count--;
+                //System.out.print(table[row][col] + "\t");
+            //}
+                //System.out.println();
 
-        }
-    }
-}
+        //}
+    //}
