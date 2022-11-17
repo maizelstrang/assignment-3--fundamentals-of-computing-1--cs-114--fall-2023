77469302dbff4fc8378dd379b8a9a428846aa5eb
CamDas <111987807+CamDas@users.noreply.github.com>
2022-11-17 16:48:24 -0500

    Final Iteration
    Should all work

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,5 +1,18 @@
 import java.util.Scanner;
-
 public class RunMatrix {
+    public static void main (String[] args) {
 
+    int matrixSize;
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Enter Matrix size:");
+        matrixSize = scan.nextInt();
+        Matrix m1 = new Matrix(matrixSize, matrixSize);
+
+        m1.printMatrix();
+        m1.populateMatrix();
+        m1.Swap(1,1,2,2);
+        m1.printMatrix();
+        m1.FlipMatrix();
+
+    }
 }
