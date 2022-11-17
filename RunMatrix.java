be0bb2c31839e8dc56ddcc13f35b36b71242c616
Aiden Kiss <aidenk.7103@gmail.com>
2022-11-16 20:54:10 -0500

    cleaned again

--- RunMatrix.java
+++ RunMatrix.java
@@ -4,6 +4,7 @@ public class RunMatrix {
     public static void main(String[] args){
         int inputSize;
         Scanner input = new Scanner(System.in);
+
         do {
         System.out.println("\nPlease input the desired matrix size:");
         inputSize = input.nextInt();
