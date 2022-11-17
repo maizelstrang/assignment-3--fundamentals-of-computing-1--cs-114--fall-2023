1290342a6fa08c9809bad89e4df6a63efe3dd594
11jwp03 <11swp03@gmail.com>
2022-11-17 13:22:10 -0500

    move line and fix print table

--- Matrix/Matrix.java
+++ Matrix/Matrix.java
@@ -13,11 +13,10 @@ public class Matrix {
         for (int row =0;row < array.length;row++) {
             for(int col = 0;col < array[row].length;col++){
                 System.out.print(array[row][col]+"\t");
-                System.out.println();
                 }
+            System.out.println();
             }
         }
-    }
     // public void populateTable() {
     //     for (int row = 0; row < array.length; row++){
     //         for (int col = 0; col < array[row].length; col++){
@@ -25,13 +24,14 @@ public class Matrix {
     //         }
     //     }
     // }
-    // public void returnTable() {
-
-    //     for(int i=0;i<array.length;i++) {
-    //         for(int j=0;j<array[i].length;j++) {
-    //             System.out.println(array[i][j]);
-    //         }
-    //     }
-    // }
+    public void printTable() {
+        for (int row =0;row < array.length;row++) {
+            for(int col = 0;col < array[row].length;col++){
+                System.out.print(array[row][col]+"\t");
+                }
+            System.out.println();
+            }
+    }
+}
 
 //https://stackoverflow.com/questions/20519100/java-how-to-return-in-a-method-multidimensional-array-without-aliasing
