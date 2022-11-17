ebf053a5acad38f5f85ae3c3217a7be5cbb17934
Aiden Kiss <aidenk.7103@gmail.com>
2022-11-16 20:46:14 -0500

    cleaning out

--- Matrix.java
+++ Matrix.java
@@ -72,25 +72,7 @@ public class Matrix {
         }
     }
     //--------------------------------------------------------------------------------------------------
-    // public void flipMatrix(){
-    //     int diagonalHighlight = matrixSize-1;
-    //     System.out.print("\n\nFlipping Matrix...Matrix Flipped");
-    //     for (int row = 0; row < matrixSize ; row++){
-    //         System.out.println();
-    //         for (int col = 0; col < matrixSize; col++){
-    //             if (diagonalHighlight == col){
-    //             System.out.printf("%s%d%s", COLOR, matrix[row][col], "\t");
-    //             diagonalHighlight--;
-    //             } else {
-    //                 swap(row, col, matrixSize-1, matrixSize-1);
-    //                 System.out.printf("%s%d%s", RESET, matrix[row][col], "\t");
 
-    //             }
-
-    //         }
-    //     }
-    // }
-    //--------------------------------------------------------------------------------------------------
     public void flipMatrix(){
         int diagonalHighlight = matrixSize-1;
         int x2 = matrixSize-1;
