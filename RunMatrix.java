b7cc0d66dc284e337ed0074092406f648a43836b
11jwp03 <11swp03@gmail.com>
2022-11-17 13:13:47 -0500

    add new table

--- RunMatrix.java
+++ RunMatrix.java
@@ -3,15 +3,16 @@ import Matrix.Matrix;
 
 public class RunMatrix {
     public static void main(String[] args){
+        Matrix table1;
 
         Scanner SizeScan = new Scanner(System.in);
 
         System.out.print("Please enter the size of your matrix: ");
         String SizeInput = SizeScan.nextLine();
+        table1 = new Matrix(5);
         System.out.println();
         System.out.println();
         System.out.println("The size of your matrix is: " +
                             SizeInput + " x " + SizeInput);
-        createTable(5);
     }
 }
