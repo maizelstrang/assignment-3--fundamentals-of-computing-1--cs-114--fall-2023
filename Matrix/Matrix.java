106bfb69e5d427c7c2854636451e04cd8ecb09d7
11jwp03 <11swp03@gmail.com>
2022-11-17 13:16:06 -0500

    fix brackest

--- Matrix/Matrix.java
+++ Matrix/Matrix.java
@@ -19,21 +19,20 @@ public class Matrix {
             }
         }
     }
-    public void populateTable() {
-        for (int row = 0; row < array.length; row++){
-            for (int col = 0; col < array[row].length; col++){
-                array[row][col] = row*10+col;
-            }
-        }
-    }
-    public void returnTable() {
+    // public void populateTable() {
+    //     for (int row = 0; row < array.length; row++){
+    //         for (int col = 0; col < array[row].length; col++){
+    //             array[row][col] = row*10+col;
+    //         }
+    //     }
+    // }
+    // public void returnTable() {
 
-        for(int i=0;i<array.length;i++) {
-            for(int j=0;j<array[i].length;j++) {
-                System.out.println(array[i][j]);
-            }
-        }
-    }
-}
+    //     for(int i=0;i<array.length;i++) {
+    //         for(int j=0;j<array[i].length;j++) {
+    //             System.out.println(array[i][j]);
+    //         }
+    //     }
+    // }
 
 //https://stackoverflow.com/questions/20519100/java-how-to-return-in-a-method-multidimensional-array-without-aliasing
