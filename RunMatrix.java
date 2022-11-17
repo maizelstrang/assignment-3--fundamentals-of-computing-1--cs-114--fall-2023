5b54aa63eca41d7f08a3e8297bc294023ff52eef
stevenbehrens <bobgiblets@gmail.com>
2022-11-17 00:53:29 -0500

    Finished assignment

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 
 public class RunMatrix {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String args[]){
+        System.out.println("Please enter a number for your Matrix.");
+        int size = input.nextInt();
+        Matrix matrix = new Matrix(size);
+        matrix.displayMatrix();
+        System.out.println();
+        matrix.fillMatrix();
+        matrix.displayMatrix();
+        System.out.println();
+        matrix.fillMatrix();
+        matrix.flipMatrix();
 
+
+    }
 }
