b0b2145552c6a69d32feeea4c6aa0a507f59bfb4
11jwp03 <11swp03@gmail.com>
2022-11-17 13:01:39 -0500

    remove clutter

--- RunMatrix.java
+++ RunMatrix.java
@@ -3,7 +3,6 @@ import Matrix.Matrix;
 
 public class RunMatrix {
     public static void main(String[] args){
-        ArrayList<String> al = new ArrayList<String>();
 
         Scanner SizeScan = new Scanner(System.in);
 
@@ -13,11 +12,6 @@ public class RunMatrix {
         System.out.println();
         System.out.println("The size of your matrix is: " +
                             SizeInput + " x " + SizeInput);
-        // table1.returnTable();
-        char[][] twoDarray = returnTable(al);
-    }
-
-    private static char[][] returnTable(ArrayList<String> al) {
-        return null;
+        createTable(5);
     }
 }
