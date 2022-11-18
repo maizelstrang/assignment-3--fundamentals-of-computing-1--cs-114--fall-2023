42487cae1c5cad3e6270768b46bf15e5abd70911
Aiden Kiss <aidenk.7103@gmail.com>
2022-11-17 19:21:50 -0500

    removed unneccessary code

--- Matrix.java
+++ Matrix.java
@@ -65,74 +65,7 @@ public class Matrix {
             }
         }
     }
-    //--------------------------------------------------------------------------------------------------
 
-    // public void flipMatrix(){
-    //     int diagonalHighlight = matrix.length-1;
-    //     int x2 = matrixSize-1;
-    //     int x1 = 0;
-
-    //     System.out.print("\n\nFlipping Matrix...Matrix Flipped");
-
-    //     //This algorithm is for the odd matrix
-    //     // if (matrixSize%2 == 1){
-
-    //         for (int row = 0; row < matrixSize ; row++){
-    //             int y2 = matrixSize-1;
-    //             int y1 = 0;
-    //             System.out.println();
-
-    //             for (int col = 0; col < matrixSize; col++){
-    //                 if (diagonalHighlight == col){
-    //                     System.out.printf("%s%d%s", COLOR, matrix[row][col], "\t");
-    //                     diagonalHighlight--;
-    //                 } else {
-
-    //                     swap(x1, y1, x2, y2);
-    //                     y2--;
-    //                     y1++;
-    //                     System.out.printf("%s%d%s", RESET, matrix[row][col], "\t");
-
-    //                 }
-    //             }
-    //             x1++;
-    //             x2--;
-    //         }
-
-    //     //Algorithm for an even matrix size
-    //     // }
-    //     // else {
-    //         // for (int row = 0; row < matrixSize ; row++){
-    //         //     int y2 = matrixSize-1;
-    //         //     int y1 = 0;
-    //         //     System.out.println();
-
-    //         //     for (int col = 0; col < matrixSize; col++){
-    //         //         if (diagonalHighlight == col){
-    //         //             System.out.printf("%s%d%s", COLOR, matrix[row][col], "\t");
-    //         //             diagonalHighlight--;
-    //         //             // if (y1-1 == y2 && x1+1 == x2){
-    //         //             //     swap(x1, y1, x2, y2);
-    //         //             // }
-
-    //         //         } else {
-    //         //             swap(x1, y1, x2, y2);
-    //         //             y2--;
-    //         //             y1++;
-    //         //             System.out.printf("%s%d%s", RESET, matrix[row][col], "\t");
-    //         //             if (x1 == (matrixSize/2)-1 && y2 == matrixSize-1){
-    //         //                 matrix[row][col]++;
-    //         //                 System.out.printf("%s%d%s", RESET, matrix[row][col], "\t");
-    //         //             }
-
-    //         //         }
-    //         //     }
-    //         //     x1++;
-    //         //     x2--;
-    //         // }
-    //     // }
-
-    // }
     //--------------------------------------------------------------------------------------------------
     public void flipMatrixV1(){
         for (int col = 0; col < (matrixSize-1); col++){
